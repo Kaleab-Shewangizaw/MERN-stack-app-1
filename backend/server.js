05d66fb50b0089adf024db989fe22ede38deb7d0
Kaleab-Shewangizaw <kaleab.stk@gmail.com>
2024-10-20 02:36:19 +0300

    server.js added and added the type: module

---
+++ backend/server.js
@@ -0,0 +1,5 @@
+// const express = require('express'); instead of this we can use modern way by adding "type": "module", in the package.json
+
+// so the other way will be:
+
+import express from "express";
