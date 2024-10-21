c113a0d655ce1a498bf3b38f83b989213cc0cb81
Kaleab-Shewangizaw <kaleab.stk@gmail.com>
2024-10-21 04:20:39 +0300

    started the api

--- backend/server.js
+++ backend/server.js
@@ -5,19 +5,43 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("<h1>API is ready</h1>");
+app.use(express.json()); // to parse the incoming request with JSON payloads (middleware). simply: it allows us to use req.body in our routes
+
+app.post("/api/products", async (req, res) => {
+  const product = req.body; //user will send this data
+
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all fields" });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({
+      success: true,
+      message: "Product created successfully",
+      data: newProduct,
+    });
+  } catch (error) {
+    console.error(error.message);
+    return res
+      .status(500)
+      .json({ success: false, message: "Server error while creating product" });
+  }
 });
 
-console.log(process.env.MONGO_URI);
-app.listen(3000, () => {
+app.listen(5001, () => {
   connectDB(); // connecting to MongoDB server
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port 5001");
 });
 
 // v4COjj9MsK44p8rj
